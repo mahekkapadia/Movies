3b915a9070f1d64007a1749806f531c544f04201
Mahek Kapadia <kapadiamahek7@gmail.com>
2026-09-21 18:01:46 +0530

    Refactor Navbar component structure and styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,50 +1,35 @@
 import React from 'react';
-
 import { Link, useNavigate } from 'react-router-dom';
-
 import { useDispatch, useSelector } from 'react-redux';
 
-
 function Navbar() {
-
     const navigate = useNavigate();
     const dispatch = useDispatch();
-
     const user = useSelector(
         (state) => state.user
     );
 
-
     const handleLogout = () => {
-
         localStorage.removeItem('movieUser');
-
         dispatch({
             type: 'LOGOUT'
         });
-
         navigate('/');
     };
 
-
     return (
-
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-
             <div className="container">
-
+                
                 {/* Logo */}
-
                 <Link
                     className="navbar-brand fw-bold"
                     to="/"
                 >
-                    🎬 Movie Library
+                    Movie Library
                 </Link>
 
-
                 {/* Mobile button */}
-
                 <button
                     className="navbar-toggler"
                     type="button"
@@ -54,60 +39,44 @@ function Navbar() {
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
-
                 <div
                     className="collapse navbar-collapse"
                     id="navbarMenu"
                 >
 
                     {/* Menu */}
-
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-
                         <li className="nav-item">
-
                             <Link
                                 className="nav-link"
                                 to="/"
                             >
                                 Popular Movies
                             </Link>
-
                         </li>
 
-
                         <li className="nav-item">
-
                             <Link
                                 className="nav-link"
                                 to="/search"
                             >
                                 Search
                             </Link>
-
                         </li>
 
-
                         <li className="nav-item">
-
                             <Link
                                 className="nav-link"
                                 to="/manage"
                             >
                                 Manage Movies
                             </Link>
-
                         </li>
-
                     </ul>
 
-
                     {/* Sign In / Sign Out */}
-
                     <div className="d-flex">
-
                         {user ? (
-
                             <button
                                 className="btn btn-outline-light"
                                 onClick={handleLogout}
@@ -116,14 +85,12 @@ function Navbar() {
                             </button>
 
                         ) : (
-
                             <Link
                                 to="/profile"
                                 className="btn btn-primary"
                             >
                                 Sign In
                             </Link>
-
                         )}
                     </div>
                 </div>
@@ -131,6 +98,4 @@ function Navbar() {
         </nav>
     );
 }
-
-
 export default Navbar;
